da1ca7da6705397825c3e918aad9799d028bd93a
AP Java Period 5 <apjavaperiod5@ac1mwk0lpjc9h.78b505.nycboe.org>
2022-10-03 12:07:42 -0400

    added fav movie files

---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class FavoriteMovie here.
+ *
+ * @author Sofia Diaz
+ * @09/21/22
+ */
+public class FavoriteMovie
+{
+  public static void main(String[] args) {
+    System.out.println("The movie Purple Hearts on Netflix is a new perspective on a romance between a conservative man in the military and an extremely liberal singer/songwriter.");
+    System.out.println("Our female main character has diabetes and a lot of student debt, and she could be described as a starving artist.");
+    System.out.print("And our male main character was enlisted in the military trying to make his father proud but also dealing with debt he had to pay to his former drug dealer.");
+    System.out.print("Our main characters meet through a mutual friend and quickly learn that they are polar opposites. Luke wants to follow the rules and support his government while Cassie will take any chance she is given to mess with the patriarchy and government.");
+    System.out.println("Cassie finds out that marine spouses get benefits that could help her pay for the diabetes medicine she desperately needs so she asks the mutual friend that Luke and her share but he refuses to marry her. Luke tells her that its illegal and she is really stupid for wanting to try it.");
+    System.out.println("Then Luke realizes he is also desperate to get more money to pay off his own debt. So they strike a deal to get married for a year and make it look as believable as possible. Things get complicated when Luke returns from his time at war early because he sustained an injury. Cassie agrees to care for him and they fall in love.");
+  }  
+    }
+
