4523429e9289e01602a09b481adc8ba52874ee7f
AP Java Period 5 <apjavaperiod5@AC1MWK0LPJC9H.local>
2022-10-20 11:48:14 -0400

    updated Usernameproject

---
+++ 02-rocketship/rocketship.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class rocketship here.
+ *
+ * @author Sofia Diaz, Lisa Ng, and Ahad Tasaddaq
+ * @version 9/23/2022
+ */
+public class rocketship
+{//class rocketship//
+    public static void main(String[]args){//main method begins//
+        cone1();
+        box1();
+        name();
+        box2();
+        cone2();
+    }
+    public static void cone1(){//first cone method begins//
+        System.out.println("  //\\");
+        System.out.println(" //  \\");
+        System.out.println("//    \\");
+    }//first cone method ends//
+    public static void box1(){//first box method begins//
+      System.out.println("+------+");
+      System.out.println("|      |");
+      System.out.println("|      |");
+      System.out.println("+------+");
+    }//end of first box method//
+    public static void name(){//name method begins//
+        System.out.println("|United|");
+        System.out.println("|States|");
+    }//end of name method//
+    public static void box2(){//begin box 2 method//
+      System.out.println("+------+");
+      System.out.println("|      |");
+      System.out.println("|      |");
+      System.out.println("+------+");
+    }//end of second box method//
+    public static void cone2(){//begin cone 2 method//
+        System.out.println("  //\\");
+        System.out.println(" //  \\");
+        System.out.println("//    \\");    
+    }//end of second cone method//
+}//end of main method//
--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.Random;
 public class UserName {
-  public static void main(String[] args) {
+  //beginning of main method//
+    public static void main(String[] args) {
     // create Scanner object
     Scanner s = new Scanner(System.in);
 
-    // get String input from user
+    // get personal info from user//
     System.out.print("Enter your first name: ");
     String firstName = s.nextLine();
     System.out.print("Enter your last name: ");
@@ -12,25 +14,42 @@ public class UserName {
     System.out.print("Enter your favorite number:");
     String favNum = s.nextLine();
     System.out.print("Are you a teacher? Enter yes or no:");
-    String studentTeacher = s.nextLine();
+    String studentTeacher = s.nextLine().toLowerCase();
+    //student or teacher//
     if (studentTeacher.equals("yes")){
     System.out.println("Hello " + initialize(firstName) + lastName +favNum+"@schools.nyc.gov");
     }
     else{
       System.out.println("Hello " + firstName + initialize(lastName) +favNum+"@nycstudents.net");  
-    }// test output
-     
+     }
+    createPassword(8);
     s.close();
-  } // end main method
-
-  /*
-   * Name: initialize
-   * Purpose: send back the first character (inital) of a name
-   * Input: a name (String)
-   * Return: a single character (String)
-   */
-  public static String initialize(String n) {
+ }
+    public static String createPassword(int length){
+       for (int i = 0; i<length; i++);
+        String password="";
+       int capital= (char)(int)(Math.random()*(90-65+1)+ 65);
+       int lowercase= (char)(int)(Math.random()*(122-97+1)+ 97);
+       int special= (char)(int)(Math.random()*(47-33+1)+ 33);
+       char c = (char)(capital);
+       char l = (char)(lowercase);
+       char s = (char)(special);
+       System.out.println("Your password is "+capital+""+c+lowercase+""+l+special+""+s);
+    //purpose: send back a random string of numbers, upper/lowercase letters//
+    //convert to char = character c = (char)i;
+    // add char to password string
+    //Input: length of password (int)//
+    //generate random number
+    //65-90 capital letters
+    //97-122 lowercase
+    //33-47 special symbols
+    //Return: String 
+        
+        
+       return password; 
+    }//end of createPassword method//
+     
+    public static String initialize(String n) {
     return n.substring(0, 1);
   } // end initialize method
-
-} // end class
+  } // end main method//
